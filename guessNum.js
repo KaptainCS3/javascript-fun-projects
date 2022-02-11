7e4ff11681955c21709b04e94540bca63f9fe1a1
KaptainCS3 <mbiapplegryn@gmail.com>
2022-02-11 23:03:02 +0100

    update file name and minor content changes

---
+++ guessNum.js
@@ -0,0 +1,8 @@
+let guessNum = prompt("Guess a the number from 1 to 10:");
+let mistryNum = Math.floor(Math.random () * 10) + 1;
+    if(guessNum == mistryNum)
+alert(`You guess correctly!! mistryNum is ${mistryNum}`);
+    else if(guestNum > 10)
+    alert("Out of range refresh and guest again!!");
+else
+alert(`Try Again!! mistryNum is ${mistryNum}`);
